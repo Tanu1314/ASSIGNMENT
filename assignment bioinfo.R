cbc2bfb93f1e506971dbb0cc3131146d4f90ea68
Tanu1314 <bhattitanya59@gmail.com>
2021-05-12 20:04:56 +0530

    assignment commit

---
+++ assignment bioinfo.R
@@ -0,0 +1,23 @@
+# BASIC ARITHEMATIC FUNCTIONS
+# addition
+7+8
+9+10
+11+23
+#subraction
+56-9
+56-23
+45-98
+#multiplication
+56*7
+7*8
+63*96
+#divsion
+98/2
+88/2
+96/2
+890/1
+#exponent
+2^6
+56^9
+12^1
+56^7
